975f6260b93e0350b78850b7ef32c33bb2e411ac
Alan Wolfe <alan.wolfe@gmail.com>
2014-06-23 21:56:30 -0700

    Faster video recording
    * GDI screen cap
    * correct window sizing

--- ECS/SystemList.h
+++ ECS/SystemList.h
@@ -46,16 +46,18 @@ SystemComponent(type, access, hint) - specifies a component needed by the system
 
 //=================================================SYSTEM LIST=================================================================
 
-SystemBegin(Camera, SYSTEM_SINGLE, "This system controls the camera.  Only one entity can control the camera at a time.")
-	SystemComponent(Bearings, SYSTEM_READONLY, "The camera reads the entity bearings to determine it's own orientation.")
-SystemEnd
-
-SystemBegin(PlayerController, SYSTEM_SINGLE, "This system translates input into player movement.  Only one entity can use the player controller at a time.")
+SystemBegin(PlayerController, SYSTEM_SINGLE, "This system translates input into desired player movement.  Only one entity can use the player controller at a time.")
 	SystemComponent(Bearings, SYSTEM_READWRITE, "The player controller modifies entity bearings based on input state.")
 SystemEnd
 
 SystemBegin(CreaturePhysics, SYSTEM_MANY, "The physics system is what lets creatures walk around the world, and also handles projectile impacts.")
 	SystemComponent(Bearings, SYSTEM_READWRITE, "The physics system uses the physics world to resolve collisions and update creature bearings")
+SystemEnd
+
+// camera has it's own internal state and it gets set up by game data supposedly, then in response to mouse movement, it will yawright and pitchup
+// also, it does move delta stuff based on player input, and "attempts to move" so does some physics stuff
+SystemBegin(Camera, SYSTEM_SINGLE, "This system sets the camera to the orientation of an entity.  Only one entity can control the camera at a time.")
+	SystemComponent(Bearings, SYSTEM_READONLY, "The camera reads the entity bearings to determine it's own orientation.")
 SystemEnd
 
 //=============================================================================================================================
--- Platform/CDirectx.cpp
+++ Platform/CDirectx.cpp
@@ -155,13 +155,11 @@ CDirectX::~CDirectX ()
 	m_wc = twc;
     RegisterClassEx( &m_wc );
 
-	int xBorder = ::GetSystemMetrics(SM_CXSIZEFRAME);
-	int yMenu = ::GetSystemMetrics(SM_CYMENU);
-	int yBorder = ::GetSystemMetrics(SM_CYSIZEFRAME);
-
-    // Create the application's window (padding by window border for uniform BB sizes across OSs)
+	// craete the window, such that the client size is the width and height requested
+	RECT wr = {0, 0, m_width, m_height};
+	AdjustWindowRect(&wr, settings.m_FullScreen ? WS_POPUP : WS_OVERLAPPEDWINDOW, FALSE);
     m_hWnd = CreateWindow( m_wc.lpszClassName, "FPS",
-							settings.m_FullScreen ? WS_POPUP : WS_OVERLAPPEDWINDOW, 0, 0, m_width + 2*xBorder, m_height+ 2*yBorder+yMenu,
+							settings.m_FullScreen ? WS_POPUP : WS_OVERLAPPEDWINDOW, 0, 0, wr.right - wr.left, wr.bottom - wr.top,
                               NULL, NULL, m_wc.hInstance, NULL );
 
 	ShowWindow(m_hWnd, settings.m_FullScreen ? SW_MAXIMIZE : SW_SHOWDEFAULT);
@@ -261,9 +259,212 @@ void CDirectX::LoadGraphicsSettings ()
 	DataSchemasXML::Load(m_graphicsSettings, "./data/gfxsettings.xml", "GfxSettings");
 }
 
+PBITMAPINFO CreateBitmapInfoStruct(HWND hwnd, HBITMAP hBmp)
+{ 
+    BITMAP bmp; 
+    PBITMAPINFO pbmi; 
+    WORD    cClrBits; 
+
+	memset(&pbmi,0,sizeof(pbmi));
+    // Retrieve the bitmap color format, width, and height.  
+    if (!GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp)) 
+	{
+		Assert_(false);
+        return pbmi;
+	}
+
+    // Convert the color format to a count of bits.  
+    cClrBits = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel); 
+    if (cClrBits == 1) 
+        cClrBits = 1; 
+    else if (cClrBits <= 4) 
+        cClrBits = 4; 
+    else if (cClrBits <= 8) 
+        cClrBits = 8; 
+    else if (cClrBits <= 16) 
+        cClrBits = 16; 
+    else if (cClrBits <= 24) 
+        cClrBits = 24; 
+    else cClrBits = 32; 
+
+    // Allocate memory for the BITMAPINFO structure. (This structure  
+    // contains a BITMAPINFOHEADER structure and an array of RGBQUAD  
+    // data structures.)  
+
+     if (cClrBits < 24) 
+         pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+                    sizeof(BITMAPINFOHEADER) + 
+                    sizeof(RGBQUAD) * (1<< cClrBits)); 
+
+     // There is no RGBQUAD array for these formats: 24-bit-per-pixel or 32-bit-per-pixel 
+
+     else 
+         pbmi = (PBITMAPINFO) LocalAlloc(LPTR, 
+                    sizeof(BITMAPINFOHEADER)); 
+
+    // Initialize the fields in the BITMAPINFO structure.  
+
+    pbmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER); 
+    pbmi->bmiHeader.biWidth = bmp.bmWidth; 
+    pbmi->bmiHeader.biHeight = bmp.bmHeight; 
+    pbmi->bmiHeader.biPlanes = bmp.bmPlanes; 
+    pbmi->bmiHeader.biBitCount = bmp.bmBitsPixel; 
+    if (cClrBits < 24) 
+        pbmi->bmiHeader.biClrUsed = (1<<cClrBits); 
+
+    // If the bitmap is not compressed, set the BI_RGB flag.  
+    pbmi->bmiHeader.biCompression = BI_RGB; 
+
+    // Compute the number of bytes in the array of color  
+    // indices and store the result in biSizeImage.  
+    // The width must be DWORD aligned unless the bitmap is RLE 
+    // compressed. 
+    pbmi->bmiHeader.biSizeImage = ((pbmi->bmiHeader.biWidth * cClrBits +31) & ~31) /8
+                                  * pbmi->bmiHeader.biHeight; 
+    // Set biClrImportant to 0, indicating that all of the  
+    // device colors are important.  
+     pbmi->bmiHeader.biClrImportant = 0; 
+     return pbmi; 
+ } 
+
+void CreateBMPFile(HWND hwnd, const char *pszFile, PBITMAPINFO pbi, 
+                  HBITMAP hBMP, HDC hDC) 
+ { 
+     HANDLE hf;                 // file handle  
+    BITMAPFILEHEADER hdr;       // bitmap file-header  
+    PBITMAPINFOHEADER pbih;     // bitmap info-header  
+    LPBYTE lpBits;              // memory pointer  
+    DWORD dwTotal;              // total count of bytes  
+    DWORD cb;                   // incremental count of bytes  
+    BYTE *hp;                   // byte pointer  
+    DWORD dwTmp; 
+
+    pbih = (PBITMAPINFOHEADER) pbi; 
+    lpBits = (LPBYTE) GlobalAlloc(GMEM_FIXED, pbih->biSizeImage);
+
+    if (!lpBits) 
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    // Retrieve the color table (RGBQUAD array) and the bits  
+    // (array of palette indices) from the DIB.  
+    if (!GetDIBits(hDC, hBMP, 0, (WORD) pbih->biHeight, lpBits, pbi, 
+        DIB_RGB_COLORS)) 
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    // Create the .BMP file.  
+    hf = CreateFileA(pszFile, 
+                   GENERIC_READ | GENERIC_WRITE, 
+                   (DWORD) 0, 
+                    NULL, 
+                   CREATE_ALWAYS, 
+                   FILE_ATTRIBUTE_NORMAL, 
+                   (HANDLE) NULL); 
+    if (hf == INVALID_HANDLE_VALUE) 
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    hdr.bfType = 0x4d42;        // 0x42 = "B" 0x4d = "M"  
+    // Compute the size of the entire file.  
+    hdr.bfSize = (DWORD) (sizeof(BITMAPFILEHEADER) + 
+                 pbih->biSize + pbih->biClrUsed 
+                 * sizeof(RGBQUAD) + pbih->biSizeImage); 
+    hdr.bfReserved1 = 0; 
+    hdr.bfReserved2 = 0; 
+
+    // Compute the offset to the array of color indices.  
+    hdr.bfOffBits = (DWORD) sizeof(BITMAPFILEHEADER) + 
+                    pbih->biSize + pbih->biClrUsed 
+                    * sizeof (RGBQUAD); 
+
+    // Copy the BITMAPFILEHEADER into the .BMP file.  
+    if (!WriteFile(hf, (LPVOID) &hdr, sizeof(BITMAPFILEHEADER), 
+        (LPDWORD) &dwTmp,  NULL)) 
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    // Copy the BITMAPINFOHEADER and RGBQUAD array into the file.  
+    if (!WriteFile(hf, (LPVOID) pbih, sizeof(BITMAPINFOHEADER) 
+                  + pbih->biClrUsed * sizeof (RGBQUAD), 
+                  (LPDWORD) &dwTmp, ( NULL)))
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    // Copy the array of color indices into the .BMP file.  
+    dwTotal = cb = pbih->biSizeImage; 
+    hp = lpBits; 
+    if (!WriteFile(hf, (LPSTR) hp, (int) cb, (LPDWORD) &dwTmp,NULL)) 
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    // Close the .BMP file.  
+     if (!CloseHandle(hf)) 
+	 {
+		 Assert_(false);
+		 return;
+	 }
+
+    // Free memory.  
+    GlobalFree((HGLOBAL)lpBits);
+}
+
 //-----------------------------------------------------------------------------
 void CDirectX::TakeScreenshot (const char *fileName)
 {
+#if 1
+	HDC hdc = GetDC(m_hWnd); // get the desktop device context
+	HDC hDest = CreateCompatibleDC(hdc); // create a device context to use yourself
+
+	// get the height and width of the screen
+	RECT rect;
+	GetClientRect(m_hWnd,&rect);
+
+	POINT br;
+	br.x = rect.right;
+	br.y = rect.bottom;
+
+	POINT tl;
+	tl.x = rect.left;
+	tl.y = rect.top;
+
+	ClientToScreen(m_hWnd, &br);
+	ClientToScreen(m_hWnd, &tl);
+
+	int height = br.y - tl.y;//GetSystemMetrics(SM_CYVIRTUALSCREEN);
+	int width = br.x - tl.x;//GetSystemMetrics(SM_CXVIRTUALSCREEN);
+
+	// create a bitmap
+	HBITMAP hbDesktop = CreateCompatibleBitmap( hdc, width, height);
+
+	// use the previously created device context with the bitmap
+	SelectObject(hDest, hbDesktop);
+
+	// copy from the desktop device context to the bitmap device context
+	// call this once per 'frame'
+	BitBlt(hDest, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
+
+	auto a = CreateBitmapInfoStruct(m_hWnd, hbDesktop);
+	CreateBMPFile(m_hWnd, fileName, a, hbDesktop, hdc);
+
+	// after the recording is done, release the desktop context you got..
+	ReleaseDC(NULL, hdc);
+
+	// ..and delete the context you created
+	DeleteDC(hDest);
+#else
 	// backbufferSurfDesc is saved at OnResizedSwapChain call
 	ID3D10Resource *backbufferRes;
 	m_pSwapChainRTV->GetResource(&backbufferRes);
@@ -288,6 +489,7 @@ void CDirectX::TakeScreenshot (const char *fileName)
 
 	hr = D3DX10SaveTextureToFile(texture, D3DX10_IFF_BMP, fileName);
 	texture->Release();
+#endif
 }
 
 //-----------------------------------------------------------------------------
